6933b3eaa31d28776c437bec2f2e098f80028f2b
Denis0160 <denis.sescu@iisviolamarchesini.edu.it>
2025-02-05 09:18:35 +0100

    es5

--- Systemcall_esBase/es_5/es_5.c
+++ Systemcall_esBase/es_5/es_5.c
@@ -1,12 +1,12 @@
-/**
- * Si vuole realizzare in linguaggio C un software monoprocesso che implementi il seguente comando:
-$ cat file1.txt file2.txt file3.txt | grep “c”
+/*
+Si vuole realizzare in linguaggio C un software monoprocesso che implementi il seguente comando:
+$ cat file1.txt file2.txt file3.txt /tmp/file4.txt | grep “c”
 
-Dove file[N].txt con N maggiore uguale a 1 sono una serie di files e il carattere "c" è a scopo esemplificativo e potrebbe essere
-qualsiasi carattere.
+Dove file[N].txt con N maggiore uguale a 1 sono una serie di files scelti dall'utente e il carattere
+"c" è a scopo esemplificativo e potrebbe essere qualsiasi carattere.
 
 Si utilizzino le system call al fine di leggere tutti i file di testo e, per ogni file, ricercare il carattere.
-Al termine dell’esecuzione il software dovrà mostrare a video il numero di occorrenze del carattere trovate nei files e
+Al termine dell’esecuzione il software dovrà mostrare a video il numero di occorrenze trovate nei files e
 dovrà contemporaneamente scrivere l’output su un file di testo il cui path sarà letto da argv.
 
 Esempio:
@@ -15,16 +15,24 @@ $ Il carattere k compare 10 volte nei files: file1.txt, file2.txt, file3.txt
 $ cat occorrenze.txt
 $ Il carattere k compare 10 volte nei files: file1.txt, file2.txt, file3.txt
 
+Esempio:
+$ ./a.out file1.txt file2.txt file3.txt /tmp/file3.txt q /tmp/occorrenze.txt
+$ Il carattere q compare 18 volte nei files: file1.txt file2.txt file3.txt /tmp/file3.txt
+$ cat /tmp/occorrenze.txt
+$ Il carattere q compare 18 volte nei files: file1.txt file2.txt file3.txt /tmp/file3.txt
+
+
 Osservazioni:
 essendo il software monoprocesso non è necessario usare le pipe
 prestare attenzione all’uso corretto di argv
-si ricorda che il software deve funzionare per un numero arbitrario di files.
+si ricorda che il software deve funzionare per un numero arbitrario di files di input.
 i percorsi dei files possono essere sia assoluti che relativi
 
 
 Suggerimento:
 Per facilitare la ricerca è possibile leggere un singolo carattere per volta usando la funzione read.
- */
+Utilizzare la funzione strcat per concatenare due stringhe.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -66,6 +74,12 @@ int main(int argc, char *argv[])
 
     fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     write(fd, output, strlen(output));
+
+    for (int i = 0; i < argc-2; i++)
+    {
+        sprintf(output,"%s",argv[i]);
+        write(fd, output, strlen(output));
+    }
     close(fd);
 
     return 0;
