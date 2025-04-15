8352fab7068d20243d5193ca8eb4de4670053933
Denis0160 <denis.sescu@iisviolamarchesini.edu.it>
2025-04-15 16:28:27 +0200

    test

--- test/calcolatrice.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCOLATRICE_H
-#define CALCOLATRICE_H
-
-int somma(int a, int b);
-int sottrazione(int a, int b);
-int moltiplicazione(int a, int b);
-int divisione(int a, int b);
-
-#endif
